42799f0bac08db202b0b9dcc793db2f3cf597150
Dungoss <70448211+Dungoss@users.noreply.github.com>
2022-11-16 16:51:28 +0700

    setup views and several routers
    Co-Authored-By: sotreduch2602 <86106907+sotreduch2602@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/App.css';
+import HomePage from './views/HomePage';
+import Login from './views/Login';
+
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link className='link' to='/'>Home</Link>
+            </li>
+            <li>
+              <Link className='link' to='/login'>Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<HomePage />}/>  
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/views/HomePage/index.js
@@ -0,0 +1,10 @@
+//import React, { useEffect, useState } from 'react';
+import React from 'react';
+
+let HomePage = () => {
+  return (
+    <h1>Home Page</h1>
+  );
+};
+
+export default HomePage;
