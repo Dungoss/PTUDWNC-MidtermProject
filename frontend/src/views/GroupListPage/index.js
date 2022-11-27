98959d0dde6d456ccd15a15deb09063eff04dba8
Dungoss <86106907+sotreduch2602@users.noreply.github.com>
2022-11-27 09:20:45 +0700

    Add components
    Co-Authored-By: Lê Duy Dũng <70448211+Dungoss@users.noreply.github.com>

--- frontend/src/views/GroupListPage/index.js
+++ frontend/src/views/GroupListPage/index.js
@@ -1,26 +1,42 @@
 import ListGroup from 'react-bootstrap/ListGroup';
+import './index.css';
 
 function GroupListPage() {
 
 const GroupInfo = () => (
   <div>
     <ListGroup>
-    <ListGroup.Item>Cras justo odio</ListGroup.Item>
-    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-    <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-    <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    <ListGroup.Item className='item'>Cras justo odio</ListGroup.Item>
+    <ListGroup.Item className='item'>Dapibus ac facilisis in</ListGroup.Item>
+    <ListGroup.Item className='item'>Morbi leo risus</ListGroup.Item>
+    <ListGroup.Item className='item'>Porta ac consectetur ac</ListGroup.Item>
+    <ListGroup.Item className='item'>Vestibulum at eros</ListGroup.Item>
     </ListGroup>
   </div>
 )
 
   return (
     <div>
-      
-      <GroupInfo></GroupInfo>
-      <GroupInfo></GroupInfo>
-      <GroupInfo></GroupInfo>
-      <GroupInfo></GroupInfo>
+      <div className='group'>
+        <GroupInfo></GroupInfo>
+      </div>
+      <div className='group'>
+        <GroupInfo></GroupInfo>
+      </div>
+
+      <div className='group'>
+        <GroupInfo></GroupInfo>
+      </div>
+
+      <div className='group'>
+        <GroupInfo></GroupInfo>
+      </div>
+      <div className='group'>
+        <GroupInfo></GroupInfo>
+      </div>
+      <div className='group'>
+        <GroupInfo></GroupInfo>
+      </div>
 
     </div>
   );
