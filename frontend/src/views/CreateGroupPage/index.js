b92654f67b668acb093332e0ea1dbb68b99188a0
Dungoss <70448211+Dungoss@users.noreply.github.com>
2022-11-27 13:20:25 +0700

    fix create group

--- frontend/src/views/CreateGroupPage/index.js
+++ frontend/src/views/CreateGroupPage/index.js
@@ -1,96 +1,123 @@
-import React from 'react';
-//import ReactDOM from 'react-dom';
-import { Formik, Field, Form, useField, useFormikContext } from 'formik';
-import './styles.css';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+//import FormControlLabel from "@mui/material/FormControlLabel";
+//import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+//import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { registerApi } from "../../api";
+import "./index.css";
 
-async function fetchNewTextC(a, b) {
-  await new Promise((r) => setTimeout(r, 500));
-  return `textA: ${a}, textB: ${b}`;
-}
 
-const MyField = (props) => {
-  const {
-    values: { textA, textB },
-    setFieldValue,
-  } = useFormikContext();
-  const [field, meta] = useField(props);
+const theme = createTheme();
 
-  React.useEffect(() => {
-    let isCurrent = true;
-    // your business logic around when to fetch goes here.
-    if (textA.trim() !== '' && textB.trim() !== '') {
-      fetchNewTextC(textA, textB).then((textC) => {
-        if (!!isCurrent) {
-          // prevent setting old values
-          setFieldValue(props.name, textC);
-        }
-      });
+export default function CreateGroupForm() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+    const res = await registerApi({
+      username: data.get("email"),
+      password: data.get("password"),
+    });
+    console.log("res", res);
+    if (res?.success) {
+      
     }
-    return () => {
-      isCurrent = false;
-    };
-  }, [textB, textA, setFieldValue, props.name]);
+  };
 
   return (
-    <>
-      <input {...props} {...field} />
-      {!!meta.touched && !!meta.error && <div>{meta.error}</div>}
-    </>
-  );
-};
-function CreateGroupForm() {
-  const initialValues = { textA: '', textB: '', textC: '' };
+    <div className="root">
+      <div className="cover">
+        <ThemeProvider theme={theme}>
+          <Container component="main" maxWidth="xs" className="ctn">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Create your own group
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 3 }}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="mem"
+                      label="Họ và tên thành viên thứ nhất"
+                      name="member"
+                      autoComplete="name"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="mem"
+                      label="Họ và tên thành viên thứ 2"
+                      name="member"
+                      autoComplete="name"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="mem"
+                      label="Họ và tên thành viên thứ 3"
+                      name="member"
+                      autoComplete="name"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="mem"
+                      label="Tên nhóm"
+                      name="member"
+                      autoComplete="name"
+                    />
+                  </Grid>
 
-  return (
-    <div className="App">
-      <Formik
-        initialValues={initialValues}
-        onSubmit={async (values) => alert(JSON.stringify(values, null, 2))}
-      >
-        <div className="section">
-          <h1>Create Your Group</h1>
-          <p style={{ color: '#555' }}>
-            Please fill in the information of your group!
-          </p>
-          <div>
-            <small>
-              <em>
+                </Grid>
                 
-              </em>
-            </small>
-          </div>
-          <Form>
-            <label>
-              Group name    
-              <Field name="GroupName" />
-            </label>
-            <label>
-              Member's name
-              <Field name="mem1" />
-            </label>
-            <label>
-              Member's name
-              <Field name="mem2" />
-            </label>
-            <label>
-              Member's name
-              <Field name="mem3" />
-            </label>
-            
-           <label>
-              textC
-              <MyField name="textC" />
-              <small>
-                (the result of <code>fetchNewTextC(textA, textB))</code>
-              </small>
-            </label>
-            <button type="submit">Submit</button>
-          </Form>
-        </div>
-      </Formik>
+                <Button           
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >                             
+                  <Link href='/group-list' className="link">Tạo nhóm</Link>
+                </Button>
+              </Box>
+            </Box>
+          </Container>
+        </ThemeProvider>
+      </div>
     </div>
   );
 }
-
-export default CreateGroupForm;
-
